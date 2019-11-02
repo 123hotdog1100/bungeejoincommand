3dafab2eb73a4a286a6c6345bc733c85cbe09f0b
BuildTools <unconfigured@null.spigotmc.org>
2019-11-02 18:21:41 +0000

    second upload

--- BungeePlugin/src/main/java/me/BungeePlugin/BungeeMain.java
+++ BungeePlugin/src/main/java/me/BungeePlugin/BungeeMain.java
@@ -1,4 +1,11 @@
 package me.BungeePlugin;
 
-public class BungeeMain {
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class BungeeMain extends JavaPlugin {
+
+    @Override
+    public void onEnable(){
+        getLogger().info("Custom Plugin has loaded");
+    }
 }
